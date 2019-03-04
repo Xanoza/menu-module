1b40f8e20bc1c47fe7c5afe816a122da660f1a7e
Alexei Sorokine <2picae@gmail.com>
2019-03-04 11:15:08 -0800

    Add Artillery test

--- server/sdc-db/db-postgres.js
+++ server/sdc-db/db-postgres.js
@@ -2,22 +2,23 @@ const {Client} = require('pg');
 // const config = require('./config.js');
 const dbName = 'restaurants';
 const tbName = 'menus';
-const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: dbName,
-  password: 'password',
-  port: 5432
-});
 
 const findAllMenusForId = async (id, callback) => {
+  const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: dbName,
+    password: 'password',
+    port: 5432
+  });
   client.connect()
     .then(() => console.log('Connected to Postgres'))
     .catch(err => {
       console.error('MY connection error', err.stack);
       return console.log(err);
     });
-  const res = await client.query(`SELECT res_id, type, category, description, price FROM ${tbName} WHERE res_id=${id}`);
+  const res = await client.query(`SELECT res_id, type, category, description, price FROM ${tbName} WHERE res_id=${id}`)
+    .catch(err => console.log('----------', err));
   client.end();
   callback(null, res.rows);
 };
--- server/sdc-server.js
+++ server/sdc-server.js
@@ -9,10 +9,12 @@ server.use(cors());
 server.use(express.static(path.join(__dirname, '../client/dist')));
 
 server.get("/mongo/:id", function (req, res) {
+  console.log(req.params.id)
   const id = parseInt(req.params.id);
   dbMongo.findAllMenusForId(id, function (err, data) {
     if (err) {
       console.log("error message: ", err);
+      res.status(400).end();
       return;
     }
     res.send(data);
@@ -20,10 +22,12 @@ server.get("/mongo/:id", function (req, res) {
 });
 
 server.get("/pg/:id", function (req, res) {
+  console.log(req.params.id)
   const id = parseInt(req.params.id);
   dbPostgres.findAllMenusForId(id, function (err, data) {
     if (err) {
       console.log("error message: ", err);
+      res.status(400).end();
       return;
     }
     res.send(data);
